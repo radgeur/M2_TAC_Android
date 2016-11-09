636f8161bceba99b76d8e7816eaad57412500328
radgeur <remy2112@hotmail.fr>
2016-11-09 15:26:07 +0100

    tp0 on intents works

--- TP0_intent/app/src/main/java/application/lepretre/tp_intent/MainActivity.java
+++ TP0_intent/app/src/main/java/application/lepretre/tp_intent/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MONCODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,43 @@ public class MainActivity extends AppCompatActivity {
         ((Button) findViewById(R.id.next)).setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                //startActivityForResult permet de savoir si le retour c'est bien ou mal passé
-                //put extra permet de récupérer la valeur
                 Intent i_rate = new Intent(v.getContext(), rating.class);
-                startActivity(i_rate);
+                float vote_initial = 2;
+                i_rate.putExtra("vote_initial", vote_initial);
+                startActivityForResult(i_rate, MONCODE);
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        float vote;
+
+		switch (requestCode) {
+            case MONCODE:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "Merci d'avoir voté",
+                                Toast.LENGTH_SHORT).show();
+
+                        vote = data.getFloatExtra("vote", -100);
+                        Toast.makeText(getApplicationContext(),
+                                "vous avez voté " + vote, Toast.LENGTH_SHORT).show();
+                        break;
+                    case RESULT_CANCELED:
+                        // pour solution 2, 3 et 4
+                        Toast.makeText(getApplicationContext(),
+                                "Pourquoi ne pas avoir voté ?", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
--- TP0_intent/app/src/main/java/application/lepretre/tp_intent/rating.java
+++ TP0_intent/app/src/main/java/application/lepretre/tp_intent/rating.java
@@ -1,11 +1,8 @@
 package application.lepretre.tp_intent;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.RatingBar;
 
 public class rating extends AppCompatActivity {
@@ -14,9 +11,23 @@ public class rating extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+    }
 
-        ((RatingBar) findViewById(R.id.rate_me)).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.rate_me);
+
+        if (getIntent().getExtras() != null) {
+            float vote = getIntent().getExtras().getFloat("vote_initial");
+            ratingBar.setRating(vote);
+        }
+
+       ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             public void onRatingChanged(RatingBar rate,float rating, boolean fromUser){
+                Intent intent = getIntent();
+                intent.putExtra("vote", rating);
+                setResult(RESULT_OK, intent);
                 finish();
             }
         });
